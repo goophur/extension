1e91df6cafac74f09311e528c965e4f53992395f
thirty potatoes <drinkereliza@gmail.com>
2019-05-06 11:52:07 -0700

    deleted most of src

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>App goes here!</div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/* USE THIS ONE FOR PLAIN/EXTENSION FCN VIEW */
-// import App from './other/App';
-
-/* USE THIS ONE FOR COPY OF BUILD PAGE FROM SITE */
-import App from './fromSite/App';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
