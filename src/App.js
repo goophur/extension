baf588cf866fbc013e6820f57906ddba70af5a34
thirty potatoes <drinkereliza@gmail.com>
2019-05-05 17:00:59 -0700

    react linter chrome workaround

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+/* global chrome */
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          App goes here!
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    user: {},
+    isLoggedIn: false
+  }
+
+  componentDidMount() {
+    chrome.storage.sync.get(["user"], function (result) {
+      console.log(result);
+      console.log('Value currently is ' + result.key);
+  });
+}
+
+  render() {
+    return ( 
+      <div className = "App" >
+        <header className = "App-header" >
+          <p>
+            App goes here!
+          </p> 
+        </header> 
+      </div>
+    );
+  }
 }
 
 export default App;
